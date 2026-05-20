5790200e4f36e941e2c540e4ec54eb3200e3795f
tacheraSasi <tacherasasi@gmail.com>
2026-05-20 10:34:53 +0300

    remove unnecessary blank line in main function

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ fn main() {
         eprintln!("No arguments provided. Please provide at least one command.");
         std::process::exit(1);
     }
-
     println!("you have provided {} commands", args.len() - 1);
     for i in 1..args.len() {
         println!("Command {}: {}", i, args[i]);
